ad3e85f359fbe32501ce7ba56e2cb790c743f9fd
Michael Robinson <mike@pagesofinterest.net>
2015-03-30 22:12:42 +1300

    Update 02-async-pyramid-of-doom-tamed.js

--- promises/02-async-pyramid-of-doom-tamed.js
+++ promises/02-async-pyramid-of-doom-tamed.js
@@ -9,10 +9,14 @@ db.set('key1', 'value1').then(function(key) {
 }).then(function(value) {
     var str += value + ' - ';
     console.log(str);
-}).catch(SomeDBException, function(err) {
-    console.log('There was a SomeDBException');
-}).error(function(erro) {
-    console.log('There was a standard error');
+}).catch(function(err)) {
+    console.log('An exception was thrown in one of the above promises');
 });
 
 // Example from http://survivejs.com/common_problems/pyramid.html
+
+// Can easily re-order execution as required or on a client's whim
+// Error handling does not require copy-paste
+// Very little indententation - in JS generally, indentation = bad 
+//  "If you need more than 3 levels of indentation, you're screwed 
+//  anyway, and should fix your program." -- Linus
